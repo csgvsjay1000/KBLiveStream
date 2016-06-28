c80ce110c1f97403a0f33414b573812c55dc4e42
kobi_2016 <254633397@qq.com>
2016-06-28 18:41:41 +0800

    tr

---
+++ KBLiveStream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KBLiveStream
+//
+//  Created by chengshenggen on 6/28/16.
+//  Copyright © 2016 Gan Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KBLiveStream/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KBLiveStream
+//
+//  Created by chengshenggen on 6/28/16.
+//  Copyright © 2016 Gan Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
